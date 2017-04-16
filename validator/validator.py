0194c1147eaf45bfbf82037dd69a8ba23b694b9e
3ammor <o.3ammor@gmail.com>
2017-04-16 14:14:16 +0200

    AF 102

--- validator/validator.py
+++ validator/validator.py
@@ -3,8 +3,12 @@ import numpy as np
 image = np.random.randint(0, 255, size=(256, 256))
 
 with open("image", 'w') as f:
+    f.write("""// memory data file (do not edit the following line - required for mem load use)
+// instance=/koko_micro/instruction_mem_port/instruction_mem
+// format=mti addressradix=d dataradix=b version=1.0 wordsperline=1\n""")
     for i in range(256):
         for j in range(256):
+            f.write(str(i * 256 + j) + ": ")
             f.write(str(image[i, j]))
             f.write('\n')
 
@@ -15,7 +19,7 @@ column = position - row * 256
 
 print(row, column)
 
-block = image[row:row+16, column:column+16]
+block = image[row:row + 16, column:column + 16]
 
 with open("block", 'w') as f:
     for i in range(16):
@@ -35,9 +39,9 @@ total = 0
 
 for i in range(1, 17):
     for j in range(1, 17):
-        total += abs(padded[i, j]-padded[i-1, j])
-        total += abs(padded[i, j]-padded[i+1, j])
-        total += abs(padded[i, j]-padded[i, j-1])
-        total += abs(padded[i, j]-padded[i, j+1])
+        total += abs(padded[i, j] - padded[i - 1, j])
+        total += abs(padded[i, j] - padded[i + 1, j])
+        total += abs(padded[i, j] - padded[i, j - 1])
+        total += abs(padded[i, j] - padded[i, j + 1])
 
 print("total= ", total)
